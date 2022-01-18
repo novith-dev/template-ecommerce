968f68f675a7c5942fb1c0bc295f991286bdba0f
Joel Novith <97770155+novith-dev@users.noreply.github.com>
2022-01-18 14:48:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="icones/themify-icons/themify-icons.css">
+    
+    
+    <title>Document</title>
+    <script src="js/menu.js"></script>
+</head>
+<body>
 
+
+
+<div id="popup1" class="overlay">
+
+	<div class="popup">
+		<h2>Criar conta</h2>
+		<a class="close" href="#">&times;</a>
+
+		<div class="content">
+      <input class="input" type="text" name=""placeholder="Nome completo"  id="">
+	<input class="input"placeholder="email" type="email" name="" id="">
+  <input placeholder="Crie um senha"  class="input" type="password" name="" id="">
+
+<button class="btn" type="submit">Cria conta</button>
+		</div>
+	</div>
+</div>
+
+<div class="header">
+
+
+<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="ti-menu"></i>
+  </a>
+  <a href="#default" class="logo">Logo</a>
+  <div class="header-right">
+    <a href="#home">Inicio</a>
+    <a href="#popup1">Criar conta</a>
+    <a href="#about">Login</a>
+  </div>
+</div> 
+<div class="topnav">
+
+
+  <div id="myLinks">
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">Login</a>
+  </div>
+  </div>
+
+
+
+
+<div class="categoria">
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat ti-desktop"></i>
+</div>
+<small>Categoria</small>
+</div>
+
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat ti-tablet"></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat ti-mobile"></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat ti-game"></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat ti-basketball"></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat  ti-headphone-alt "></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat  ti-music-alt "></i>
+</div>
+<small>Categoria</small>
+</div>
+
+<div class="down">
+<div class="circulo">
+    <i class="i_cat  ti-star "></i>
+</div>
+<small>Categoria</small>
+</div>
+
+
+</div>
+
+
+
+<div class="main">
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Nome do produto</b></h4> 
+    <small>Descrição do produto</small> 
+  </div>
+<button>Comprar</button>
+</div>
+
+
+
+</div>
+
+
+<!--###########################carrossel#################### -->
+
+<div class="sell"><h3>Veja as últimas novidades selecionadas para você!</h3> 
+<div class="carrossel">
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+<div class="card">
+  <img src="img/note.JPG" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>Jane Doe</b></h4> 
+    <p>Interior Designer</p> 
+  </div>
+<button>Comprar</button>
+</div>
+
+
+
+
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                   
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Contatos</h3>
+                        <ul>
+                            <li>loja@gmail.com</li>
+                            <li>+55 99 9 9999-9999</li>
+                           
+                        </ul>
+                        <br>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Nome da loja</h3>
+                        <p>coisas sobre a loja (slogan)</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="ti-instagram"></i></a><a href="#"><i class="ti-twitter"></i></a><a href="#"><i class="ti-youtube"></i></a><a href="#"><i class="ti-facebook"></i></a></div>
+                </div>
+                <p class="copyright">Nome da loja © 2018</p>
+            </div>
+        </footer>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
