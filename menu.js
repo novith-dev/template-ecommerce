50da36b329f94e5924724fc8fff2d4faca004b54
Joel Novith <97770155+novith-dev@users.noreply.github.com>
2022-01-18 14:59:04 -0300

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,19 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function popup() {
+  var x = document.getElementBy("popup");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
